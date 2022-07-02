3c7c93f93b8076d7bec83f075f0fb9bcd4720d05
AnushriArjeeth <104078706+AnushriArjeeth@users.noreply.github.com>
2022-07-02 15:37:20 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+
+Function(fun) 
+{
+    var links=["girl.jpg","boy.webp","mom.jpg","dad.jpg",];
+    var names=["Anushri.J","Arjeeth.J","Mom","Dad",];
+
+    for(i=0;i<links;i++)
+        {
+            console.log(links[i]);
+
+            links=links+names[i]+"<br>";
+            
+        }
+
+    
+}
+
+
